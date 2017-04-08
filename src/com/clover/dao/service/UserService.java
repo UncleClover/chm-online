8896bddaece41f96538bde951158897d2df68af3
lucky-clover <qiang900714@126.com>
2017-04-09 01:16:57 +0800

    用户接口

---
+++ src/com/clover/dao/service/UserService.java
@@ -0,0 +1,23 @@
+package com.clover.dao.service;
+
+import java.util.HashMap;
+
+/**
+ * @desc 用户接口类
+ * @copyright Copyright (c) 2017
+ * @company clover
+ * @version V1.0
+ * @author qiang900714@126.com
+ * @date 2017-4-9 上午1:06:39
+ */
+public interface UserService {
+	/**
+	 * @desc 登录查询
+	 * @author zhangdq
+	 * @time 2017-4-9 上午1:12:00
+	 * @param userAccount 登录账户
+	 * @param passWord 登录密码
+	 * @return HashMap<String, String>
+	 */
+	HashMap<String, String> queryUser(String userAccount, String passWord);
+}
