9489767564030b4aa26e23673ca1eed82f3c5256
zhangdq <zhangdq@zhangdq-PC>
2017-05-25 23:18:34 +0800

    提交代码

--- src/com/clover/base/jdbc/session/Session.java
+++ src/com/clover/base/jdbc/session/Session.java
@@ -1,5 +1,98 @@
 package com.clover.base.jdbc.session;
 
-public abstract interface Session {
+import java.sql.Connection;
+import java.util.List;
 
+import com.clover.base.jdbc.DataRow;
+
+/**
+ * @desc 数据库连接会话操作接口
+ * @author zhangdq
+ */
+public abstract interface Session {
+	/**
+	 * @desc 获取数据库连接
+	 * @author zhangdq
+	 * @return Connection
+	 */
+	public abstract Connection connection();
+
+	/**
+	 * @desc 插入表
+	 * @param tbName
+	 *            表名
+	 * @param DataRow
+	 *            插入的列
+	 * @return int
+	 */
+	public abstract int insert(String tbName, DataRow data);
+
+	/**
+	 * @desc 根据条件删除表数据
+	 * @author zhangdq
+	 * @param tbName
+	 *            表名
+	 * @param identify
+	 *            删除条件的列名
+	 * @param identifyValue
+	 *            删除条件的值
+	 * @return int
+	 */
+	public abstract int delete(String tbName, String identify, Object identifyValue);
+
+	/**
+	 * @desc 更新表数据
+	 * @author zhangdq
+	 * @param sql
+	 *            更新SQL语句
+	 * @param objs
+	 *            参数
+	 * @return int
+	 */
+	public abstract int update(String sql, Object[] objs);
+
+	/**
+	 * @desc 查询数据
+	 * @author zhangdq
+	 * @param sql
+	 *            查询SQL
+	 * @return List<DataRow>
+	 */
+	public abstract List<DataRow> query(String sql);
+
+	/**
+	 * @desc 查询数据、
+	 * @author zhangdq
+	 * @param sql
+	 *            查询SQL
+	 * @param objs
+	 *            查询参数
+	 * @return List<DataRow>
+	 */
+	public abstract List<DataRow> query(String sql, Object[] objs);
+
+	/**
+	 * @desc 开启数据库事务
+	 * @author zhangdq
+	 * @time 2017-05-25 22:57
+	 */
+	public abstract void beginTrans();
+
+	/**
+	 * @desc 提交数据库事务
+	 * @author zhangdq
+	 */
+	public abstract void commitTrans();
+
+	/**
+	 * @desc 回滚数据库事务操作
+	 * @author zhangdq
+	 */
+	public abstract void rollbackTrans();
+
+	/**
+	 * @desc 关闭数据库连接
+	 * @author zhangdq
+	 */
+	public abstract void close();
 }
--- src/com/clover/base/jdbc/session/impl/SessionImpl.java
+++ src/com/clover/base/jdbc/session/impl/SessionImpl.java
@@ -1,7 +1,95 @@
 package com.clover.base.jdbc.session.impl;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.clover.base.jdbc.DataRow;
 import com.clover.base.jdbc.session.Session;
 
-public class SessionImpl implements Session{
+public class SessionImpl implements Session {
+	private Connection conn = null;
+
+	public SessionImpl(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public Connection connection() {
+		return conn;
+	}
+
+	@Override
+	public int insert(String tbName, DataRow data) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO ");
+		sb.append(tbName).append(" ");
+		sb.append("(");
+		int i = 0;
+		List valList = new ArrayList();
+		String replaceStr = "";
+		for(Iterator it = data.keySet().iterator();it.hasNext();){
+			String columnName = (String) it.next();
+			valList.add(data.get(columnName));
+			if(i < data.size()){
+				sb.append(columnName).append(",");
+				replaceStr = replaceStr + "?,";
+			}else{
+				sb.append(columnName).append(") ");
+				replaceStr = replaceStr + "?";
+			}
+		}
+		sb.append("values(").append(replaceStr).append(")");
+		return 0;
+	}
+
+	@Override
+	public int delete(String tbName, String identify, Object identifyValue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(String sql, Object[] objs) {
+		
+		return 0;
+	}
+
+	@Override
+	public List<DataRow> query(String sql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DataRow> query(String sql, Object[] objs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void beginTrans() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void commitTrans() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rollbackTrans() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
