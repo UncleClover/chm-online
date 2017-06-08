eb451d9f67bee4760c141f8f19c1eeefe29f67ec
lucky-clover <qiang900714@126.com>
2017-06-08 13:42:25 +0800

    注解更改

--- src/com/clover/base/jdbc/session/Session.java
+++ src/com/clover/base/jdbc/session/Session.java
@@ -7,19 +7,19 @@ import com.clover.base.jdbc.DBPage;
 import com.clover.base.jdbc.DataRow;
 
 /**
- * @desc 数据库连接会话操作接口
+ * 数据库连接会话操作接口
  * @author zhangdq
  */
 public abstract interface Session {
 	/**
-	 * @desc 获取数据库连接
+	 * 获取数据库连接
 	 * @author zhangdq
 	 * @return Connection
 	 */
 	public abstract Connection connection();
 
 	/**
-	 * @desc 插入表
+	 * 插入表
 	 * @param tbName 表名
 	 * @param DataRow 插入的列
 	 * @return int
@@ -27,7 +27,7 @@ public abstract interface Session {
 	public abstract int insert(String tbName, DataRow data);
 
 	/**
-	 * @desc 根据条件删除表数据
+	 * 根据条件删除表数据
 	 * @author zhangdq
 	 * @param tbName 表名
 	 * @param identify 删除条件的列名
@@ -37,9 +37,9 @@ public abstract interface Session {
 	public abstract int delete(String tbName, String identify, Object identifyValue);
 
 	/**
-	 * @desc 根据SQL和条件删除表数据(可直接调用update方法)
+	 * 根据SQL和条件删除表数据(可直接调用update方法)
 	 * @author zhangdq
-	 * @time 2017-6-2 下午3:12:25
+	 * @date 2017-6-2 下午3:12:25
 	 * @param SQL 删除SQL语句
 	 * @param args 删除参数
 	 * @return int
@@ -47,16 +47,16 @@ public abstract interface Session {
 	public abstract int delete(String sql, Object[] args);
 
 	/**
-	 * @desc 更新表数据
+	 * 更新表数据
 	 * @author zhangdq
-	 * @time 2017-6-2 下午6:09:43
+	 * @date 2017-6-2 下午6:09:43
 	 * @param
 	 * @return
 	 */
 	public abstract int update(String sql);
 
 	/**
-	 * @desc 根据条件更新表数据
+	 * 根据条件更新表数据
 	 * @author zhangdq
 	 * @param sql 更新SQL语句
 	 * @param args 参数
@@ -65,9 +65,9 @@ public abstract interface Session {
 	public abstract int update(String sql, Object[] args);
 
 	/**
-	 * @desc 根据表名和一定的条件更新某些列数据
+	 * 根据表名和一定的条件更新某些列数据
 	 * @author zhangdq
-	 * @time 2017-6-2 下午6:15:54
+	 * @date 2017-6-2 下午6:15:54
 	 * @param tbName 表名
 	 * @param data 列数据
 	 * @param identify 更新条件列名
@@ -77,9 +77,9 @@ public abstract interface Session {
 	public abstract int update(String tbName, DataRow data, String identify, Object identifyValue);
 
 	/**
-	 * @desc 根据表名和一定的条件更新某些列数据
+	 * 根据表名和一定的条件更新某些列数据
 	 * @author zhangdq
-	 * @time 2017-6-2 下午6:15:54
+	 * @date 2017-6-2 下午6:15:54
 	 * @param tbName 表名
 	 * @param data 列数据
 	 * @param identifys 更新条件列名
@@ -89,18 +89,18 @@ public abstract interface Session {
 	public abstract int update(String tbName, DataRow data, String[] identifys, Object[] identifyValues);
 
 	/**
-	 * @desc 批更新数据
+	 * 批更新数据
 	 * @author zhangdq
-	 * @time 2017-6-6 下午4:44:44
+	 * @date 2017-6-6 下午4:44:44
 	 * @param sql 更新SQL的数组
 	 * @return int[]
 	 */
 	public abstract int[] batchUpdate(String[] sql);
 	
 	/**
-	 * @desc 根据条件批更新数据
+	 * 根据条件批更新数据
 	 * @author zhangdq
-	 * @time 2017-6-6 下午4:45:14
+	 * @date 2017-6-6 下午4:45:14
 	 * @param sql 更新SQL
 	 * @param args 查询条件的二维数组
 	 * @return int[]
@@ -108,7 +108,7 @@ public abstract interface Session {
 	public abstract int[] batchUpdate(String sql, Object[][] args);
 	
 	/**
-	 * @desc 查询数据
+	 * 查询数据
 	 * @author zhangdq
 	 * @param sql 查询SQL
 	 * @return List<DataRow>
@@ -116,7 +116,7 @@ public abstract interface Session {
 	public abstract List<DataRow> query(String sql);
 
 	/**
-	 * @desc 查询数据、
+	 * 查询数据、
 	 * @author zhangdq
 	 * @param sql 查询SQL
 	 * @param args 查询参数
@@ -125,9 +125,9 @@ public abstract interface Session {
 	public abstract List<DataRow> query(String sql, Object[] args);
 
 	/**
-	 * @desc 查询数据前几行
+	 * 查询数据前几行
 	 * @author zhangdq
-	 * @time 2017-6-2 下午4:10:08
+	 * @date 2017-6-2 下午4:10:08
 	 * @param SQL 查询SQL
 	 * @param rows 查询行数
 	 * @return List<DataRow>
@@ -135,18 +135,18 @@ public abstract interface Session {
 	public abstract List<DataRow> query(String sql, int rows);
 
 	/**
-	 * @desc 根据条件查询数据前几行
+	 * 根据条件查询数据前几行
 	 * @author zhangdq
-	 * @time 2017-6-2 下午4:17:45
+	 * @date 2017-6-2 下午4:17:45
 	 * @param
 	 * @return
 	 */
 	public abstract List<DataRow> query(String sql, Object[] args, int rows);
 
 	/**
-	 * @desc 从某一行开始查询一定行数的数据
+	 * 从某一行开始查询一定行数的数据
 	 * @author zhangdq
-	 * @time 2017-6-2 下午4:12:42
+	 * @date 2017-6-2 下午4:12:42
 	 * @param SQL 查询SQL
 	 * @param startRows 开始查询位置
 	 * @param rows 查询行数
@@ -155,9 +155,9 @@ public abstract interface Session {
 	public abstract List<DataRow> query(String sql, int startRows, int rows);
 
 	/**
-	 * @desc 根据条件从某一行开始查询一定行数的数据
+	 * 根据条件从某一行开始查询一定行数的数据
 	 * @author zhangdq
-	 * @time 2017-6-2 下午4:13:49
+	 * @date 2017-6-2 下午4:13:49
 	 * @param SQL 查询SQL
 	 * @param args 查询条件参数
 	 * @param startRows 开始查询位置
@@ -167,18 +167,18 @@ public abstract interface Session {
 	public abstract List<DataRow> query(String sql, Object[] args, int startRows, int rows);
 
 	/**
-	 * @desc 查询int类型数据
+	 * 查询int类型数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:52:37
+	 * @date 2017-6-5 下午5:52:37
 	 * @param sql 查询SQL
 	 * @return int
 	 */
 	public abstract int queryInt(String sql);
 
 	/**
-	 * @desc 根据条件查询int类型数据
+	 * 根据条件查询int类型数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:53:13
+	 * @date 2017-6-5 下午5:53:13
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return int
@@ -186,18 +186,18 @@ public abstract interface Session {
 	public abstract int queryInt(String sql, Object[] args);
 
 	/**
-	 * @desc 查询int数组
+	 * 查询int数组
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:53:33
+	 * @date 2017-6-5 下午5:53:33
 	 * @param sql 查询SQL
 	 * @return int[]
 	 */
 	public abstract int[] queryIntArray(String sql);
 
 	/**
-	 * @desc 根据条件查询int数组
+	 * 根据条件查询int数组
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:53:56
+	 * @date 2017-6-5 下午5:53:56
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return int[]
@@ -205,18 +205,18 @@ public abstract interface Session {
 	public abstract int[] queryIntArray(String sql, Object[] args);
 
 	/**
-	 * @desc 查询long类型数据
+	 * 查询long类型数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:57:47
+	 * @date 2017-6-5 下午5:57:47
 	 * @param sql 查询SQL
 	 * @return long
 	 */
 	public abstract long queryLong(String sql);
 
 	/**
-	 * @desc 根据条件查询查询long类型数据
+	 * 根据条件查询查询long类型数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:58:17
+	 * @date 2017-6-5 下午5:58:17
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return long
@@ -224,18 +224,18 @@ public abstract interface Session {
 	public abstract long queryLong(String sql, Object[] args);
 
 	/**
-	 * @desc 查询long数组数据
+	 * 查询long数组数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:58:59
+	 * @date 2017-6-5 下午5:58:59
 	 * @param sql 查询SQL
 	 * @return long[]
 	 */
 	public abstract long[] queryLongArray(String sql);
 
 	/**
-	 * @desc 根据条件查询long数组数据
+	 * 根据条件查询long数组数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午5:59:29
+	 * @date 2017-6-5 下午5:59:29
 	 * @param sql
 	 * @param args 查询条件参数
 	 * @return
@@ -243,18 +243,18 @@ public abstract interface Session {
 	public abstract long[] queryLongArray(String sql, Object[] args);
 
 	/**
-	 * @desc 查询String类型数据
+	 * 查询String类型数据
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:02:39
+	 * @date 2017-6-5 下午6:02:39
 	 * @param sql 查询SQL
 	 * @return String
 	 */
 	public abstract String queryString(String sql);
 
 	/**
-	 * @desc 根据条件查询String类型
+	 * 根据条件查询String类型
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:03:09
+	 * @date 2017-6-5 下午6:03:09
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return String
@@ -262,18 +262,18 @@ public abstract interface Session {
 	public abstract String queryString(String sql, Object[] args);
 
 	/**
-	 * @desc 查询String数组
+	 * 查询String数组
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:03:58
+	 * @date 2017-6-5 下午6:03:58
 	 * @param sql 查询SQL
 	 * @return String[]
 	 */
 	public abstract String[] queryStringArray(String sql);
 
 	/**
-	 * @desc 根据条件查询String数组
+	 * 根据条件查询String数组
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:04:03
+	 * @date 2017-6-5 下午6:04:03
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return String[]
@@ -281,18 +281,18 @@ public abstract interface Session {
 	public abstract String[] queryStringArray(String sql, Object[] args);
 
 	/**
-	 * @desc 查询一条数据(DataRow)
+	 * 查询一条数据(DataRow)
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:04:12
+	 * @date 2017-6-5 下午6:04:12
 	 * @param sql 查询SQL
 	 * @return DataRow
 	 */
 	public abstract DataRow queryMap(String sql);
 
 	/**
-	 * @desc 根据条件查询一条数据(DataRow)
+	 * 根据条件查询一条数据(DataRow)
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:04:16
+	 * @date 2017-6-5 下午6:04:16
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @return DataRow
@@ -300,9 +300,9 @@ public abstract interface Session {
 	public abstract DataRow queryMap(String sql, Object[] args);
 
 	/**
-	 * @desc 数据分页查询
+	 * 数据分页查询
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:04:24
+	 * @date 2017-6-5 下午6:04:24
 	 * @param sql 查询SQL
 	 * @param curPage 当前页数
 	 * @param numPerPage 每页显示数据条数
@@ -311,9 +311,9 @@ public abstract interface Session {
 	public abstract DBPage queryPage(String sql, int curPage, int numPerPage);
 
 	/**
-	 * 
+	 * 根据条件分页查询
 	 * @author zhangdq
-	 * @time 2017-6-5 下午6:04:27
+	 * @date 2017-6-5 下午6:04:27
 	 * @param sql 查询SQL
 	 * @param args 查询条件参数
 	 * @param curPage 当前页数
@@ -323,26 +323,26 @@ public abstract interface Session {
 	public abstract DBPage queryPage(String sql, Object[] args, int curPage, int numPerPage);
 
 	/**
-	 * @desc 开启数据库事务
+	 * 开启数据库事务
 	 * @author zhangdq
-	 * @time 2017-05-25 22:57
+	 * @date 2017-05-25 22:57
 	 */
 	public abstract void beginTrans();
 
 	/**
-	 * @desc 提交数据库事务
+	 * 提交数据库事务
 	 * @author zhangdq
 	 */
 	public abstract void commitTrans();
 
 	/**
-	 * @desc 回滚数据库事务操作
+	 * 回滚数据库事务操作
 	 * @author zhangdq
 	 */
 	public abstract void rollbackTrans();
 
 	/**
-	 * @desc 关闭数据库连接，数据库关闭异常不做处理
+	 * 关闭数据库连接，数据库关闭异常不做处理
 	 * @author zhangdq
 	 */
 	public abstract void close();
