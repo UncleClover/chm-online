0e70c3312373947e6e9ec879538e9c6eb58b3c17
lucky-clover <qiang900714@126.com>
2017-06-13 18:16:15 +0800

    增减常量字段

--- src/com/clover/base/constants/ChmConstants.java
+++ src/com/clover/base/constants/ChmConstants.java
@@ -2,12 +2,20 @@ package com.clover.base.constants;
 
 public class ChmConstants {
 	/**
-	 * @desc 数据库配置文件
+	 * 数据库配置文件-datasource.xml
 	 * @author zhangdq
 	 * @time 2017-04-09 17:40
 	 * @value datasource.xml
 	 */
 	public final static String DATASOURCE_FILE_NAME = "datasource.xml";
+	
+	/**
+	 * 数据库异常代码，序列不存在-2289
+	 * @author zhangdq
+	 * @time 2017-06-13 18:12
+	 * @value 2289
+	 */
+	public final static int SEQUENCE_NOT_EXISTS = 2289;
 	
 	public final static String DB_TEST = "test";
 }
