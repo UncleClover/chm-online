66f64bb2095097743772123c59d226aad1e5608c
lucky-clover <qiang900714@126.com>
2017-06-13 18:24:11 +0800

    数据库类型类

---
+++ src/com/clover/base/jdbc/DatabaseType.java
@@ -0,0 +1,16 @@
+package com.clover.base.jdbc;
+
+/**
+ * 数据库类型
+ * @copyright Copyright (c) 2017
+ * @company clover
+ * @version V1.0
+ * @author qiang900714@126.com
+ * @date 2017-6-13 上午9:57:52
+ */
+public final class DatabaseType {
+	
+	public static int ORACLE = 0;
+	
+	public static int MYSQL = 1;
+}
