9ae2c6414aaa3af065bd9260b7ed1734acc2bd05
zhangdq <zhangdq@zhangdq-PC>
2017-06-13 22:25:02 +0800

    添加注释

--- src/com/clover/base/jdbc/session/JdbcTemplate.java
+++ src/com/clover/base/jdbc/session/JdbcTemplate.java
@@ -40,6 +40,12 @@ public class JdbcTemplate {
 		return SessionFactory.getSession();
 	}
 
+	/**
+	 * 插入表
+	 * @param tbName 表名
+	 * @param DataRow 插入的列
+	 * @return int
+	 */
 	public int insert(String tbName, DataRow data) {
 		Session session = null;
 		int result = 0;
@@ -56,6 +62,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件删除表数据
+	 * @author zhangdq
+	 * @param tbName 表名
+	 * @param identify 删除条件的列名
+	 * @param identifyValue 删除条件的值
+	 * @return int
+	 */
 	public int delete(String tbName, String identify, Object identifyValue) {
 		Session session = null;
 		int result = 0;
@@ -72,6 +86,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据SQL和条件删除表数据(可直接调用update方法)
+	 * @author zhangdq
+	 * @date 2017-6-2 下午3:12:25
+	 * @param SQL 删除SQL语句
+	 * @param args 删除参数
+	 * @return int
+	 */
 	public int delete(String sql, Object[] args) {
 		Session session = null;
 		int result = 0;
@@ -88,6 +110,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 更新表数据
+	 * @author zhangdq
+	 * @date 2017-6-2 下午6:09:43
+	 * @param
+	 * @return
+	 */
 	public int update(String sql) {
 		Session session = null;
 		int result = 0;
@@ -104,12 +133,19 @@ public class JdbcTemplate {
 		return result;
 	}
 
-	public int update(String sql, Object[] objs) {
+	/**
+	 * 根据条件更新表数据
+	 * @author zhangdq
+	 * @param sql 更新SQL语句
+	 * @param args 参数
+	 * @return int
+	 */
+	public int update(String sql, Object[] args) {
 		Session session = null;
 		int result = 0;
 		try {
 			session = getSession();
-			result = session.update(sql, objs);
+			result = session.update(sql, args);
 			generatedKeys = session.getGeneratedKeys();
 		} finally {
 			if (session != null) {
@@ -120,6 +156,16 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据表名和一定的条件更新某些列数据
+	 * @author zhangdq
+	 * @date 2017-6-2 下午6:15:54
+	 * @param tbName 表名
+	 * @param data 列数据
+	 * @param identify 更新条件列名
+	 * @param identifyValue 更新条件值
+	 * @return int
+	 */
 	public int update(String tbName, DataRow data, String identify, Object identifyValue) {
 		Session session = null;
 		int result = 0;
@@ -136,6 +182,16 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据表名和一定的条件更新某些列数据
+	 * @author zhangdq
+	 * @date 2017-6-2 下午6:15:54
+	 * @param tbName 表名
+	 * @param data 列数据
+	 * @param identifys 更新条件列名
+	 * @param identifyValues 更新条件值
+	 * @return int
+	 */
 	public int update(String tbName, DataRow data, String[] identifys, Object[] identifyValues) {
 		Session session = null;
 		int result = 0;
@@ -152,6 +208,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 批更新数据
+	 * @author zhangdq
+	 * @date 2017-6-6 下午4:44:44
+	 * @param sql 更新SQL的数组
+	 * @return int[]
+	 */
 	public int[] batchUpdate(String[] sql) {
 		Session session = null;
 		int[] result = null;
@@ -167,6 +230,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件批更新数据
+	 * @author zhangdq
+	 * @date 2017-6-6 下午4:45:14
+	 * @param sql 更新SQL
+	 * @param args 查询条件的二维数组
+	 * @return int[]
+	 */
 	public int[] batchUpdate(String sql, Object[][] args) {
 		Session session = null;
 		int[] result = null;
@@ -182,6 +253,12 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询数据
+	 * @author zhangdq
+	 * @param sql 查询SQL
+	 * @return List<DataRow>
+	 */
 	public List<DataRow> query(String sql) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
@@ -197,12 +274,19 @@ public class JdbcTemplate {
 		return list;
 	}
 
-	public List<DataRow> query(String sql, Object[] objs) {
+	/**
+	 * 查询数据
+	 * @author zhangdq
+	 * @param sql 查询SQL
+	 * @param args 查询参数
+	 * @return List<DataRow>
+	 */
+	public List<DataRow> query(String sql, Object[] args) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
 		try {
 			session = getSession();
-			list = session.query(sql, objs);
+			list = session.query(sql, args);
 		} finally {
 			if (session != null) {
 				session.close();
@@ -212,6 +296,14 @@ public class JdbcTemplate {
 		return list;
 	}
 
+	/**
+	 * 查询数据前几行
+	 * @author zhangdq
+	 * @date 2017-6-2 下午4:10:08
+	 * @param SQL 查询SQL
+	 * @param rows 查询行数
+	 * @return List<DataRow>
+	 */
 	public List<DataRow> query(String sql, int rows) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
@@ -227,12 +319,19 @@ public class JdbcTemplate {
 		return list;
 	}
 
-	public List<DataRow> query(String sql, Object[] objs, int rows) {
+	/**
+	 * 根据条件查询数据前几行
+	 * @author zhangdq
+	 * @date 2017-6-2 下午4:17:45
+	 * @param
+	 * @return
+	 */
+	public List<DataRow> query(String sql, Object[] args, int rows) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
 		try {
 			session = getSession();
-			list = session.query(sql, objs, rows);
+			list = session.query(sql, args, rows);
 		} finally {
 			if (session != null) {
 				session.close();
@@ -242,6 +341,15 @@ public class JdbcTemplate {
 		return list;
 	}
 
+	/**
+	 * 从某一行开始查询一定行数的数据
+	 * @author zhangdq
+	 * @date 2017-6-2 下午4:12:42
+	 * @param SQL 查询SQL
+	 * @param startRows 开始查询位置
+	 * @param rows 查询行数
+	 * @return List<DataRow>
+	 */
 	public List<DataRow> query(String sql, int startRows, int rows) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
@@ -257,12 +365,22 @@ public class JdbcTemplate {
 		return list;
 	}
 
-	public List<DataRow> query(String sql, Object[] objs, int startRows, int rows) {
+	/**
+	 * 根据条件从某一行开始查询一定行数的数据
+	 * @author zhangdq
+	 * @date 2017-6-2 下午4:13:49
+	 * @param SQL 查询SQL
+	 * @param args 查询条件参数
+	 * @param startRows 开始查询位置
+	 * @param rows 查询行数
+	 * @return List<DataRow>
+	 */
+	public List<DataRow> query(String sql, Object[] args, int startRows, int rows) {
 		Session session = null;
 		List<DataRow> list = new ArrayList<DataRow>();
 		try {
 			session = getSession();
-			list = session.query(sql, objs, startRows, rows);
+			list = session.query(sql, args, startRows, rows);
 		} finally {
 			if (session != null) {
 				session.close();
@@ -272,6 +390,13 @@ public class JdbcTemplate {
 		return list;
 	}
 
+	/**
+	 * 查询int类型数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:52:37
+	 * @param sql 查询SQL
+	 * @return int
+	 */
 	public int queryInt(String sql) {
 		Session session = null;
 		int result = 0;
@@ -287,6 +412,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询int类型数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:53:13
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return int
+	 */
 	public int queryInt(String sql, Object[] args) {
 		Session session = null;
 		int result = 0;
@@ -302,6 +435,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询int数组
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:53:33
+	 * @param sql 查询SQL
+	 * @return int[]
+	 */
 	public int[] queryIntArray(String sql) {
 		Session session = null;
 		int[] result = null;
@@ -317,6 +457,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询int数组
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:53:56
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return int[]
+	 */
 	public int[] queryIntArray(String sql, Object[] args) {
 		Session session = null;
 		int[] result = null;
@@ -332,6 +480,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询long类型数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:57:47
+	 * @param sql 查询SQL
+	 * @return long
+	 */
 	public long queryLong(String sql) {
 		Session session = null;
 		long result = 0L;
@@ -347,6 +502,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询查询long类型数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:58:17
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return long
+	 */
 	public long queryLong(String sql, Object[] args) {
 		Session session = null;
 		long result = 0L;
@@ -362,6 +525,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询long数组数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:58:59
+	 * @param sql 查询SQL
+	 * @return long[]
+	 */
 	public long[] queryLongArray(String sql) {
 		Session session = null;
 		long[] result = null;
@@ -377,6 +547,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询long数组数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午5:59:29
+	 * @param sql
+	 * @param args 查询条件参数
+	 * @return
+	 */
 	public long[] queryLongArray(String sql, Object[] args) {
 		Session session = null;
 		long[] result = null;
@@ -392,6 +570,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询String类型数据
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:02:39
+	 * @param sql 查询SQL
+	 * @return String
+	 */
 	public String queryString(String sql) {
 		Session session = null;
 		String result = null;
@@ -407,6 +592,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询String类型
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:03:09
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return String
+	 */
 	public String queryString(String sql, Object[] args) {
 		Session session = null;
 		String result = null;
@@ -422,6 +615,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询String数组
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:03:58
+	 * @param sql 查询SQL
+	 * @return String[]
+	 */
 	public String[] queryStringArray(String sql) {
 		Session session = null;
 		String[] result = null;
@@ -437,6 +637,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询String数组
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:04:03
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return String[]
+	 */
 	public String[] queryStringArray(String sql, Object[] args) {
 		Session session = null;
 		String[] result = null;
@@ -452,6 +660,13 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 查询一条数据(DataRow)
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:04:12
+	 * @param sql 查询SQL
+	 * @return DataRow
+	 */
 	public DataRow queryMap(String sql) {
 		Session session = null;
 		DataRow result = new DataRow();
@@ -467,6 +682,14 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件查询一条数据(DataRow)
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:04:16
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @return DataRow
+	 */
 	public DataRow queryMap(String sql, Object[] args) {
 		Session session = null;
 		DataRow result = new DataRow();
@@ -482,6 +705,15 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 数据分页查询
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:04:24
+	 * @param sql 查询SQL
+	 * @param curPage 当前页数
+	 * @param numPerPage 每页显示数据条数
+	 * @return DBPage
+	 */
 	public DBPage queryPage(String sql, int curPage, int numPerPage) {
 		Session session = null;
 		DBPage result = null;
@@ -497,6 +729,16 @@ public class JdbcTemplate {
 		return result;
 	}
 
+	/**
+	 * 根据条件分页查询
+	 * @author zhangdq
+	 * @date 2017-6-5 下午6:04:27
+	 * @param sql 查询SQL
+	 * @param args 查询条件参数
+	 * @param curPage 当前页数
+	 * @param numPerPage 每页显示数据条数
+	 * @return DBPage
+	 */
 	public DBPage queryPage(String sql, Object[] args, int curPage, int numPerPage) {
 		Session session = null;
 		DBPage result = null;
@@ -512,6 +754,13 @@ public class JdbcTemplate {
 		return result;
 	}
 	
+	/**
+	 * 获取插入时自动生成的主键
+	 * @author zhangdq
+	 * @date 2017-6-13 下午4:30:44
+	 * @param 
+	 * @return
+	 */
 	public String getgeneratedKeys(){
 		return generatedKeys;
 	}
