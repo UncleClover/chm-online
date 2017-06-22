a495c6f907c850e66cd8e28a8493d2b9b488b5f0
lucky-clover <qiang900714@126.com>
2017-06-22 16:34:36 +0800

    action包调整

--- src/com/clover/action/ChmAction.java
+++ src/com/clover/action/ChmAction.java
@@ -1,14 +1,18 @@
-package com.clover.action.views;
+package com.clover.action;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
-
 import com.opensymphony.xwork2.ActionSupport;
 
 @Results({ @Result(name = "success", location = "home/index.ftl") })
 public class ChmAction extends ActionSupport {
+	private HttpServletRequest request = ServletActionContext.getRequest();
 
 	public String execute() {
+		request.setAttribute("name", "zhangdq");
 		return SUCCESS;
 	}
 }
