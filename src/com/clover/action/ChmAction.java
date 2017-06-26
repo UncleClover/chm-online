c44f45807a574298c384b6f7d9f92477af42d583
lucky-clover <qiang900714@126.com>
2017-06-26 18:15:25 +0800

    reqest获取调整为Struts2工具类

--- src/com/clover/action/ChmAction.java
+++ src/com/clover/action/ChmAction.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.springside.modules.web.struts2.Struts2Utils;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 @Results({ @Result(name = "success", location = "home/index.ftl") })
 public class ChmAction extends ActionSupport {
-	private HttpServletRequest request = ServletActionContext.getRequest();
+	private HttpServletRequest request = Struts2Utils.getRequest();
 
 	public String execute() {
 		request.setAttribute("name", "zhangdq");
