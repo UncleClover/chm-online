1383f56d1ea40e1b725aa87fe739effb05ca4a1e
lucky-clover <qiang900714@126.com>
2017-06-26 18:16:21 +0800

    增加登录方法

--- src/com/clover/action/login/LoginAction.java
+++ src/com/clover/action/login/LoginAction.java
@@ -1,15 +1,26 @@
 package com.clover.action.login;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.springside.modules.web.struts2.Struts2Utils;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@Results({ @Result(name = "success", location = "login/login.ftl") })
+@Results({ @Result(name = "success", location = "login.ftl") })
 public class LoginAction extends ActionSupport {
-	
+	Logger logger = Logger.getLogger(LoginAction.class);
+	HttpServletRequest request = Struts2Utils.getRequest();
+
 	public String execute() {
-		
 		return SUCCESS;
+	}
+
+	public String doLogin() {
+		String account = request.getParameter("account");
+		String password = request.getParameter("pwd");
+		return "login";
 	}
 }
