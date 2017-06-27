6360bdbe6cc910307b25875cb971e4d370747ac9
lucky-clover <qiang900714@126.com>
2017-06-27 16:46:53 +0800

    service基类

---
+++ src/com/clover/base/service/BaseService.java
@@ -0,0 +1,24 @@
+package com.clover.base.service;
+
+import com.clover.base.jdbc.session.JdbcTemplate;
+
+/**
+ * service基类
+ * @copyright Copyright (c) 2017
+ * @company clover
+ * @version V1.0
+ * @author qiang900714@126.com
+ * @date 2017-6-27 下午2:54:57
+ */
+public class BaseService {
+	/**
+	 * 根据DataSource id获取数据库连接
+	 * @author zhangdq
+	 * @date 2017-6-27 下午2:55:40
+	 * @param id
+	 * @return JdbcTemplate
+	 */
+	public JdbcTemplate getJdbcTemplate(String id) {
+		return new JdbcTemplate(id);
+	}
+}
