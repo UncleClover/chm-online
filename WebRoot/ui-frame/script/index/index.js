1de9b8c18496ca5a34bdc0c63c6eaac61f66b4da
zhangdq <zhangdq@asiainfo.com>
2017-07-18 16:15:44 +0800

    采用sea开发JS

--- WebRoot/ui-frame/script/index/index.js
+++ WebRoot/ui-frame/script/index/index.js
@@ -3,62 +3,77 @@
  * @author zhangdq
  * @time 2017-06-22 16:25
  */
-$(function(){
-	// 左侧导航目录、索引和搜索切换
-	$("#main-menu li a").click(function(){
-		var _currClass = $(this).attr("class");
-		if(_currClass && _currClass.indexOf("current") > -1){
-			return;
-		}
-		$("#main-menu li a").removeClass("current");
-		$(this).addClass("current");
-		
-		// 左导航内容切换
-		var menu_id = $(this).attr("id");
-		if(menu_id == "context_tab"){
-			$("#index,#search").hide();
-			$("#context").show();
-		}
-		
-		if(menu_id == "index_tab"){
-			$("#context,#search").hide();
-			$("#index").show();
-		}
-		
-		if(menu_id == "search_tab"){
-			$("#index,#context").hide();
-			$("#search").show();
-		}
-	});
+define("/ui-frame/script/index/index",function(require, exports, module){
+	require("jquery");
 	
-	// 左导航索引搜索
-	$("#index-input").bind("keyup", function(event) {
-		if (event.keyCode == "13") {
-			var content = $(this).val();
-			if(content){
-				content = content.replace(/\s/g,"");
+	function init(){
+		
+	}
+	
+	// 事件处理
+	function events(){
+		// 左侧导航目录、索引和搜索切换
+		$("#main-menu li a").click(function(){
+			var _currClass = $(this).attr("class");
+			if(_currClass && _currClass.indexOf("current") > -1){
+				return;
+			}
+			$("#main-menu li a").removeClass("current");
+			$(this).addClass("current");
+			
+			// 左导航内容切换
+			var menu_id = $(this).attr("id");
+			if(menu_id == "context_tab"){
+				$("#index,#search").hide();
+				$("#context").show();
+			}
+			
+			if(menu_id == "index_tab"){
+				$("#context,#search").hide();
+				$("#index").show();
+			}
+			
+			if(menu_id == "search_tab"){
+				$("#index,#context").hide();
+				$("#search").show();
+			}
+		});
+		
+		// 左导航索引搜索
+		$("#index-input").bind("keyup", function(event) {
+			if (event.keyCode == "13") {
+				var content = $(this).val();
 				if(content){
-					$(this).val(content);
+					content = content.replace(/\s/g,"");
+					if(content){
+						$(this).val(content);
+					}
 				}
 			}
-		}
-	});
-	
-	// 左导航搜索
-	$("#search-input").bind("keyup", function(event) {
-		if (event.keyCode == "13") {
-			var content = $(this).val();
-			if(content){
-				content = content.replace(/\s/g,"");
+		});
+		
+		// 左导航搜索
+		$("#search-input").bind("keyup", function(event) {
+			if (event.keyCode == "13") {
+				var content = $(this).val();
 				if(content){
-					$(this).val(content);
+					content = content.replace(/\s/g,"");
+					if(content){
+						$(this).val(content);
+					}
 				}
 			}
-		}
-	});
+		});
+		
+		// 新增chm
+		$("#add_chm").click(function(){
+			location.href = "/views/article.action";
+		});
+	};
 	
-	// 新增chm
-	$("#add_chm").click(function(){
-		location.href = "/views/article.action";
-	});
+	var index = {
+		"init" : init,
+		"events" : events	
+	};
+	module.exports = index;
 });
