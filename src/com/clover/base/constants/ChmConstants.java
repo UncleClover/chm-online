5c372a4e007661df1cc2490ff2ea89689a64173e
zhangdq <zhangdq@asiainfo.com>
2017-07-19 10:33:16 +0800

    新增操作结果返回码和信息提示

--- src/com/clover/base/constants/ChmConstants.java
+++ src/com/clover/base/constants/ChmConstants.java
@@ -25,5 +25,37 @@ public class ChmConstants {
 	 */
 	public final static int SEQUENCE_NOT_EXISTS = 2289;
 	
+	/**
+	 * 操作成功代码
+	 * @author zhangdq
+	 * @time 2017-07-19 10:23
+	 * @value 000000
+	 */
+	public final static String OPER_SUCCESS_CODE = "000000";
+	
+	/**
+	 * 操作成功信息
+	 * @author zhangdq
+	 * @time 2017-07-19 10:23
+	 * @value 操作成功
+	 */
+	public final static String OPER_SUCCESS_MSG = "操作成功";
+	
+	/**
+	 * 操作失败代码
+	 * @author zhangdq
+	 * @time 2017-07-19 10:24
+	 * @value 999999
+	 */
+	public final static String OPER_FAIL_CODE = "999999";
+	
+	/**
+	 * 操作失败信息
+	 * @author zhangdq
+	 * @time 2017-07-19 10:25
+	 * @value OPER_FAIL_CODE
+	 */
+	public final static String OPER_FAIL_MSG = "操作失败";
+	
 	public final static String DB_TEST = "test";
 }
