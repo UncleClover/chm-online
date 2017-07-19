9a496ecf4cea1ffcb904c83a463eef8998786761
zhangdq <zhangdq@asiainfo.com>
2017-07-19 10:34:55 +0800

    保存chm

--- src/com/clover/action/controller/ArticleController.java
+++ src/com/clover/action/controller/ArticleController.java
@@ -1,16 +1,46 @@
 package com.clover.action.controller;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.clover.base.BaseResponse;
+import com.clover.base.constants.ChmConstants;
 import com.clover.base.constants.TemplateConstants;
+import com.clover.base.jdbc.DataRow;
+import com.clover.dao.service.ArticleService;
 
 @Controller
 @RequestMapping("article")
 public class ArticleController {
 	
+	@Autowired
+	private ArticleService service;
+	
 	@RequestMapping("add")
 	public String addArticle() {
 		return TemplateConstants.CHM_ARTICLE_ADD;
+	}
+	
+	@RequestMapping("save")
+	@ResponseBody
+	public BaseResponse saveArticle(String title, String content){
+		if(StringUtils.isEmpty(title)){
+			return new BaseResponse(ChmConstants.OPER_FAIL_CODE, "chm标题不能为空");
+		}
+		if(StringUtils.isEmpty(content)){
+			return new BaseResponse(ChmConstants.OPER_FAIL_CODE, "chm内容不能为空");
+		}
+		
+		DataRow data = new DataRow();
+		data.set("title", title);
+		data.set("content", content);
+		int row = service.addArticle(data);
+		
+		BaseResponse result = new BaseResponse(ChmConstants.OPER_SUCCESS_CODE, ChmConstants.OPER_SUCCESS_MSG);
+		result.setResult(row);
+		return result;
 	}
 }
